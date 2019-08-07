266997ec14389c0a1f6cb83ec8e4744a38004e8b
Gustavo Morales <gustavo.morales@gmail.com>
2019-08-07 09:44:29 -0500

    Add default middleware to handle no match routes

--- server/index.js
+++ server/index.js
@@ -1,29 +1,28 @@
-/*
- * Modularizar la aplicación de Express
- * dejandole solo la responsabilidad de
- * manejar nuestra aplicación
- * independientemente del servidor Web
- * que la gestione
- */
 const express = require('express');
 
-/*
- * Inicializamos la aplicación de Express
- */
 const app = express();
 
-/*
- * Declaramos la ruta raiz / con el verbo
- * HTTP GET para cuando llegue la petición
- * ejecute el callback asociado con los
- * respectivos request (req) y response (res)
- * enviados por el servidor Web, dando como
- * respuesta y finalizando la petición con un
- * texto. A este callback Express lo llama
- * middleware
- */
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+/*
+ * Agregamos un middleware al final despues
+ * de haber declarado todas las rutas para
+ * que si ninguna ruta coincide con la de la
+ * petición Express ejecute este middleware
+ * como última medida para que nosotros podamos
+ * enviarle una respuesta personalizada al
+ * usuario con el codigo HTTP 404 que
+ * corresponde a recurso no encontrado y un
+ * mensaje en formato JSON y esto finaliza la
+ * petición
+ */
+app.use((req, res, next) => {
+  res.status(404);
+  res.json({
+    message: 'Error. Route not found',
+  });
 });
 
 module.exports = app;
