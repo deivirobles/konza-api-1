7ab0acd2de44ace1b2daff6160429f9bcfb71284
Gustavo Morales <gustavo.morales@gmail.com>
2019-08-07 16:14:10 -0500

    Versioning API

--- server/index.js
+++ server/index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const requestId = require('express-request-id')();
 
 const logger = require('./config/logger');
-const api = require('./api');
+/*
+ * Actualizamos la ruta del enrutador pues
+ * esta definiendo la version del API con
+ * el directorio v1 creado
+ */
+const api = require('./api/v1');
 
 const app = express();
 
@@ -12,13 +17,19 @@ app.use(logger.requests);
 
 // Setup router and routes
 /*
- * Utilizamos el metodo use para establecer
- * un prefijo en las rutas de la aplicación
- * en este caso /api y como segundo parametro
- * un enrutador en este caso el definido en
- * el archivo server/api/index.js
+ * Es muy importante versionar el API para
+ * evitar futuros inconvenientes con las
+ * actualizaciones.
+ * Es muy facil añadir otro prefijo a nuestro
+ * enrutador ya que esa es la forma como el
+ * funciona, ahora tenemos dos prefijos
+ * independientes que esta enlazados al mismo
+ * enrutador, lo cual es una ventaja enorme
+ * al no tener que duplicar el codigo de todas
+ * las rutas que definamos en el API
  */
 app.use('/api', api);
+app.use('/api/v1', api);
 
 // No route found handler
 app.use((req, res, next) => {
