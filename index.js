35a9e7bb977164fd5cab8be62c218312a9272fa5
Gustavo Morales <gustavo.morales@gmail.com>
2019-08-07 09:23:59 -0500

    Modularize Express and Add config file

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+/*
+ * Importar nuestra aplicación de Express y
+ * el archivo de configuración
+ */
+const app = require('./server');
+const config = require('./server/config');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+/*
+ * Extraemos la llave port del objeto server
+ * en el objeto de la configuración
+ */
+const { port } = config.server;
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+/*
+ * Reemplazamos el callback de la función
+ * createServer con la aplicación de Express
+ * ya que esta diseñada para ser compatible
+ * pues sera la encargada de manejar todas
+ * las peticiones que lleguen a nuestro
+ * servidor Web
+ */
+const server = http.createServer(app);
+
+/*
+ * El encargado de iniciar el servidor Web es
+ * el modulo http de Node.js
+ */
+server.listen(port, () => {
+  console.log(`Server running at port: ${port}`);
 });
---
+++ server/config/index.js
@@ -0,0 +1,12 @@
+/*
+ * Crear un archivo centralizado con todos los
+ * posibles valores configurables en la
+ * aplicación
+ */
+const config = {
+  server: {
+    port: 3000,
+  },
+};
+
+module.exports = config;
