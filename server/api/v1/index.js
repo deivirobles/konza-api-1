541605ace625376eeef689fadac823830e1e47ed
Gustavo Morales <gustavo.morales@gmail.com>
2019-08-07 16:32:17 -0500

    Define REST API contract for tasks resource

--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -2,22 +2,6 @@ const router = require('express').Router();
 
 const tasks = require('./tasks/routes');
 
-/*
- * Repetimos el mismo proceso que hicimos
- * al definir el prefijo (/api) para todas
- * las rutas de la aplicación, esta vez
- * definimos el prefijo /tasks para las
- * rutas que esten definidas en el enrutador
- * del recurso tasks en el archivo de routes
- * pero este a su vez tambien tiene el prefijo
- * definido en el archivo principal por lo
- * tanto las rutas se armar añadiendo todos
- * los prefijos, ej: (/api)(/tasks)(/) donde
- * los dos primeros grupos de parentesis son
- * los prefijos añadidos con el metodo use y
- * el ultimo esta definido en el enrutador
- * del recurso tasks en routes
- */
 router.use('/tasks', tasks);
 
 module.exports = router;
--- server/api/v1/tasks/routes.js
+++ server/api/v1/tasks/routes.js
@@ -1,16 +1,37 @@
 const router = require('express').Router();
 
 /*
- * Repetimos el mismo proceso esta vez para
- * las rutas del recurso tasks, es decir,
- * creamos un nuevo enrutador que manejara
- * las rutas de este recurso independientemente
- * el prefijo que se le asigne
+ * /api/tasks/ POST - CREATE
+ * /api/tasks/ GET - READ ALL
+ * /api/tasks/:id GET - READ ONE
+ * /api/tasks/:id PUT - UPDATE
+ * /api/tasks/:id DELETE - DELETE
  */
-router.get('/', (req, res, next) => {
-  res.json({
-    message: 'Get all tasks',
-  });
-});
+
+/*
+ * Definimos todas las rutas para el recurso
+ * de tasks basado en el contrato de REST
+ * API utilizando el Router y el metodo
+ * route con los verbos HTTP y middlewares
+ * correspondientes
+ */
+router
+  .route('/')
+  .post((req, res, next) => {})
+  .get((req, res, next) => {});
+
+/*
+ * Las rutas de express permiten crear
+ * parametros dinamicos utilizando los dos
+ * puntos (:) seguido del nombre del
+ * parametro, es decir que todo lo que este
+ * despues del slash (/) se convierte en
+ * el valor del parametro id y es una cadena
+ */
+router
+  .route('/:id')
+  .get((req, res, next) => {})
+  .put((req, res, next) => {})
+  .delete((req, res, next) => {});
 
 module.exports = router;
