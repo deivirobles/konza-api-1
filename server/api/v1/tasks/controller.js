3cfbf15dce9d5667f81f18dc4552ecad6eaa5cbf
Gustavo Morales <gustavo.morales@gmail.com>
2019-08-07 21:43:49 -0500

    Standarize response

--- server/api/v1/tasks/controller.js
+++ server/api/v1/tasks/controller.js
@@ -1,42 +1,84 @@
 /*
- * En cada uno de los middleware creamos las
- * variables respectivas dependiendo de la
- * informacion que esta establecida para esa
- * ruta asociada (id) o enviada por el usuario
- * (body) estas extraidas de la peticion y
- * asinandoles un valor por defecto en el caso
- * que no existan en el objeto req.
+ * Utilizamos la libreria http-status-codes para
+ * establecer los codigos de respuesta por nombre
+ * y no por numeros.
+ * Estandarizamos la respuesta creando un llave
+ * llamada data que contendra los datos que el
+ * usuario require, pero tambien meta informacion
+ * sobre el resultado de la operacion en la llave
+ * success y aunque el codigo de la respuesta esta
+ * en el mismo objeto de respuesta tambien la
+ * adjuntamos como otra llave en el objeto de
+ * respuesta
  */
+
+const HTTP_STATUS_CODE = require('http-status-codes');
+
 exports.create = (req, res, next) => {
   const { body = {} } = req;
-  res.json(body);
+
+  const doc = body;
+
+  res.status(HTTP_STATUS_CODE.CREATED);
+  res.json({
+    data: doc,
+    success: true,
+    statusCode: HTTP_STATUS_CODE.CREATED,
+  });
 };
 
 exports.all = (req, res, next) => {
-  res.json([]);
+  const docs = [];
+
+  res.json({
+    data: docs,
+    success: true,
+    statusCode: HTTP_STATUS_CODE.OK,
+  });
 };
 
 exports.read = (req, res, next) => {
   const { params = {} } = req;
   const { id } = params;
-  res.json({
+
+  const doc = {
     id,
+  };
+
+  res.json({
+    data: doc,
+    success: true,
+    statusCode: HTTP_STATUS_CODE.OK,
   });
 };
 
 exports.update = (req, res, next) => {
   const { body = {}, params = {} } = req;
   const { id } = params;
-  res.json({
+
+  const doc = {
+    ...body,
     id,
-    body,
+  };
+
+  res.json({
+    data: doc,
+    success: true,
+    statusCode: HTTP_STATUS_CODE.OK,
   });
 };
 
 exports.delete = (req, res, next) => {
   const { params = {} } = req;
   const { id } = params;
-  res.json({
+
+  const doc = {
     id,
+  };
+
+  res.json({
+    data: doc,
+    success: true,
+    statusCode: HTTP_STATUS_CODE.OK,
   });
 };
