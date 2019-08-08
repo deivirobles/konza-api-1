d6b702a86f85e753f5be46126b5cbb3ce17fad77
Gustavo Morales <gustavo.morales@gmail.com>
2019-08-08 16:48:46 -0500

    Add Schema to Task and update Error Middleware

--- server/api/v1/tasks/model.js
+++ server/api/v1/tasks/model.js
@@ -1,19 +1,49 @@
 const mongoose = require('mongoose');
 
+const { Schema } = mongoose;
+
 /*
- * Definimos los nombres y el tipo de los campos
- * del modelo y finalmente utilizamos el metodo
- * model de mongoose para declarar este modelo
- * es importante nombrar que este se crea una
- * sola vez en la aplicación no importa el numero
- * de veces que se importe este modulo. Es un
- * Singleton
+ * El Schema de mongoose nos brinda muchas
+ * funcionalidades como establecer una validacion
+ * basica para el modelo, asi como funciones de
+ * quitar los espacios de sobra en los valores de
+ * los campos y demas, inclusive colocar un valor
+ * por defecto.
  */
-const task = {
-  title: String,
-  description: String,
-  completed: Boolean,
-  dueDate: Date,
+
+const fields = {
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 128,
+  },
+  description: {
+    type: String,
+    default: '',
+    trim: true,
+    maxlength: 256,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  dueDate: {
+    type: Date,
+    default: null,
+  },
 };
+
+/*
+ * El Schema tambien permite establecer ciertas
+ * opciones una de ellas son los timestamps para
+ * que se creen los campos createdAt y updatedAt
+ * de tipo fecha cada vez que se cree o actualice
+ * un registro respectivamente
+ */
+
+const task = new Schema(fields, {
+  timestamps: true,
+});
 
 module.exports = mongoose.model('task', task);
