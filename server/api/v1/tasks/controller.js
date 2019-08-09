9e9a46e618b34525d03d83b0094242b77d3b5ff3
Gustavo Morales <gustavo.morales@gmail.com>
2019-08-09 07:31:01 -0500

    Refactor callback to Promises in Tasks controller

--- server/api/v1/tasks/controller.js
+++ server/api/v1/tasks/controller.js
@@ -3,50 +3,54 @@ const HTTP_STATUS_CODE = require('http-status-codes');
 const Model = require('./model');
 
 exports.id = (req, res, next, id) => {
-  Model.findById(id).exec((err, doc) => {
-    if (err) {
+  Model.findById(id)
+    .exec()
+    .then((doc) => {
+      if (doc) {
+        req.doc = doc;
+        next();
+      } else {
+        next({
+          message: 'Resource not found',
+          statusCode: HTTP_STATUS_CODE.NOT_FOUND,
+        });
+      }
+    })
+    .catch((err) => {
       next(err);
-    } else if (doc) {
-      req.doc = doc;
-      next();
-    } else {
-      next({
-        message: 'Resource not found',
-        statusCode: HTTP_STATUS_CODE.NOT_FOUND,
-      });
-    }
-  });
+    });
 };
 
 exports.create = (req, res, next) => {
   const { body = {} } = req;
 
-  Model.create(body, (err, doc) => {
-    if (err) {
-      next(err);
-    } else {
+  Model.create(body)
+    .then((doc) => {
       res.status(HTTP_STATUS_CODE.CREATED);
       res.json({
         data: doc,
         success: true,
         statusCode: HTTP_STATUS_CODE.CREATED,
       });
-    }
-  });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
 
 exports.all = (req, res, next) => {
-  Model.find().exec((err, docs) => {
-    if (err) {
-      next(err);
-    } else {
+  Model.find()
+    .exec()
+    .then((docs) => {
       res.json({
         data: docs,
         success: true,
         statusCode: HTTP_STATUS_CODE.OK,
       });
-    }
-  });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
 
 exports.read = (req, res, next) => {
@@ -59,48 +63,38 @@ exports.read = (req, res, next) => {
   });
 };
 
-/*
- * Para actualizar el documento utilizamos el
- * metodo save del mismo documento no el Model
- * como tal, adicionalmente sobreescribimos las
- * llaves que envia el usuario en el body con
- * el metodo Object.assign
- */
 exports.update = (req, res, next) => {
   const { body = {}, doc } = req;
 
   Object.assign(doc, body);
 
-  doc.save((err, updated) => {
-    if (err) {
-      next(err);
-    } else {
+  doc
+    .save()
+    .then((updated) => {
       res.json({
         data: updated,
         success: true,
         statusCode: HTTP_STATUS_CODE.OK,
       });
-    }
-  });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
 
-/*
- * El codigo es muy similiar a lo realizado anteriormente
- * la principal diferencia esta en el nombre del metodo
- * el cual es remove
- */
 exports.delete = (req, res, next) => {
   const { doc } = req;
 
-  doc.remove((err, deleted) => {
-    if (err) {
-      next(err);
-    } else {
+  doc
+    .remove()
+    .then((deleted) => {
       res.json({
         data: deleted,
         success: true,
         statusCode: HTTP_STATUS_CODE.OK,
       });
-    }
-  });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
