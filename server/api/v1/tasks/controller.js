09f718d26555aca6b5404027e79f8b227fcef1d7
Gustavo Morales <gustavo.morales@gmail.com>
2019-08-09 07:34:02 -0500

    Refactor Promises to Async / Await in Tasks

--- server/api/v1/tasks/controller.js
+++ server/api/v1/tasks/controller.js
@@ -2,55 +2,52 @@ const HTTP_STATUS_CODE = require('http-status-codes');
 
 const Model = require('./model');
 
-exports.id = (req, res, next, id) => {
-  Model.findById(id)
-    .exec()
-    .then((doc) => {
-      if (doc) {
-        req.doc = doc;
-        next();
-      } else {
-        next({
-          message: 'Resource not found',
-          statusCode: HTTP_STATUS_CODE.NOT_FOUND,
-        });
-      }
-    })
-    .catch((err) => {
-      next(err);
-    });
+exports.id = async (req, res, next, id) => {
+  try {
+    const doc = await Model.findById(id).exec();
+    if (doc) {
+      req.doc = doc;
+      next();
+    } else {
+      next({
+        message: 'Resource not found',
+        statusCode: HTTP_STATUS_CODE.NOT_FOUND,
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
-exports.create = (req, res, next) => {
+exports.create = async (req, res, next) => {
   const { body = {} } = req;
 
-  Model.create(body)
-    .then((doc) => {
-      res.status(HTTP_STATUS_CODE.CREATED);
-      res.json({
-        data: doc,
-        success: true,
-        statusCode: HTTP_STATUS_CODE.CREATED,
-      });
-    })
-    .catch((err) => {
-      next(err);
+  try {
+    const doc = await Model.create(body);
+
+    res.status(HTTP_STATUS_CODE.CREATED);
+    res.json({
+      data: doc,
+      success: true,
+      statusCode: HTTP_STATUS_CODE.CREATED,
     });
+  } catch (err) {
+    next(err);
+  }
 };
 
-exports.all = (req, res, next) => {
-  Model.find()
-    .exec()
-    .then((docs) => {
-      res.json({
-        data: docs,
-        success: true,
-        statusCode: HTTP_STATUS_CODE.OK,
-      });
-    })
-    .catch((err) => {
-      next(err);
+exports.all = async (req, res, next) => {
+  try {
+    const docs = await Model.find().exec();
+
+    res.json({
+      data: docs,
+      success: true,
+      statusCode: HTTP_STATUS_CODE.OK,
     });
+  } catch (err) {
+    next(err);
+  }
 };
 
 exports.read = (req, res, next) => {
@@ -63,38 +60,34 @@ exports.read = (req, res, next) => {
   });
 };
 
-exports.update = (req, res, next) => {
-  const { body = {}, doc } = req;
+exports.update = async (req, res, next) => {
+  try {
+    const { body = {}, doc } = req;
 
-  Object.assign(doc, body);
+    Object.assign(doc, body);
+    const updated = await doc.save();
 
-  doc
-    .save()
-    .then((updated) => {
-      res.json({
-        data: updated,
-        success: true,
-        statusCode: HTTP_STATUS_CODE.OK,
-      });
-    })
-    .catch((err) => {
-      next(err);
+    res.json({
+      data: updated,
+      success: true,
+      statusCode: HTTP_STATUS_CODE.OK,
     });
+  } catch (err) {
+    next(err);
+  }
 };
 
-exports.delete = (req, res, next) => {
-  const { doc } = req;
+exports.delete = async (req, res, next) => {
+  try {
+    const { doc } = req;
 
-  doc
-    .remove()
-    .then((deleted) => {
-      res.json({
-        data: deleted,
-        success: true,
-        statusCode: HTTP_STATUS_CODE.OK,
-      });
-    })
-    .catch((err) => {
-      next(err);
+    const deleted = await doc.remove();
+    res.json({
+      data: deleted,
+      success: true,
+      statusCode: HTTP_STATUS_CODE.OK,
     });
+  } catch (err) {
+    next(err);
+  }
 };
