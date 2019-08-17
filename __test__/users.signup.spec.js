454f42fc086951844a69cfb25e0610d84f498530
Gustavo Morales <gustavo.morales@gmail.com>
2019-08-16 20:39:24 -0500

    Refactor Async / Await Users Test

--- __test__/users.signup.spec.js
+++ __test__/users.signup.spec.js
@@ -3,13 +3,9 @@ const request = require('supertest');
 const server = require('./simple-server');
 
 describe('Users', () => {
-  test('GET List of Users', (done) => {
-    request(server)
-      .get('/api/users')
-      .expect((res) => {
-        const { body } = res;
-        expect(body.success).toBeTruthy();
-      })
-      .expect(200, done);
+  test('GET List of Users', async () => {
+    const res = await request(server).get('/api/users');
+    const { body } = res;
+    expect(body.success).toBeTruthy();
   });
 });
